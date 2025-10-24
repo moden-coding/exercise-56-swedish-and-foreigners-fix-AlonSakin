fab462c9d981159779c01e048ca4ef29ea60afaa
AlonSakin <29asakin@gannacademy.org>
2025-10-24 09:58:18 -0400

    yay 👍👍

--- src/swedish_and_foreigners.py
+++ src/swedish_and_foreigners.py
@@ -3,11 +3,16 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    pass
+    municipalities = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)["Akaa":"Äänekoski"]
+
+    municipalities = municipalities[municipalities["Share of Swedish-speakers of the population, %"] > 5]
+    municipalities = municipalities[municipalities["Share of foreign citizens of the population, %"] > 5]
+
+    return municipalities[["Population","Share of Swedish-speakers of the population, %","Share of foreign citizens of the population, %"]]
 
 
 def main():
-    swedish_and_foreigners()
+    print(swedish_and_foreigners())
 
 if __name__ == "__main__":
     main()
